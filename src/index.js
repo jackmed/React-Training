538606363e31ca9ef4456b42921cfbe7d146dcd7
jackmed <jackszy@126.com>
2020-07-11 20:45:45 +0800

    添加一个可以升序或降序显示历史记录的按钮

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={() => props.onClick()}>
-      {props.value}
-    </button>
-  );
-}
+
 
 class Board extends React.Component {
   renderOneRowSquare(rowArr) {
@@ -45,11 +39,15 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
         xAxis: null,
         yAxis: null,
-        boldStep: null
       }],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+      historyAscOrder: true,
+      boldStep: null,
     }
+
+    this.handleReverseHistory = this.handleReverseHistory.bind(this);
+    this.jumpTo = this.jumpTo.bind(this);
   }
 
   handleClick(i) {
@@ -72,6 +70,11 @@ class Game extends React.Component {
     });
   }
 
+  handleReverseHistory() {
+    this.setState({
+      historyAscOrder: !this.state.historyAscOrder
+    })
+  }
   jumpTo(step) {
     this.setState({
       stepNumber: step,
@@ -79,7 +82,6 @@ class Game extends React.Component {
       boldStep: step,
     })
   }
-
   getCoordinates(i) {
     return {
       xAxis: i %3,
@@ -92,38 +94,105 @@ class Game extends React.Component {
     const boldStep = this.state.boldStep;
     const currentSquares = history[this.state.stepNumber];
 
-    const winner = calculateWinner(currentSquares.squares);
-    let status;
-    if (winner) {
-      status = "winner: " + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    const historyInfo = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button
-            onClick={() => this.jumpTo(move)}
-            className={boldStep===move ? "selectHistory" : null}
-          >{desc}
-          </button>
-          <Coordinates move={move} step={step}/>
-        </li>
-      );
-    })
     return (
       <div className="game">
         <div className="game-board">
           <Board squares={currentSquares.squares} onClick={(i) => this.handleClick(i)}/>
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{historyInfo}</ol>
-        </div>
+        <Status currentSquares={currentSquares} stepNumber={this.state.stepNumber} xIsNext={this.state.xIsNext}/>
+        <History history={history} boldStep={boldStep} historyAscOrder={this.state.historyAscOrder} jumpTo={this.jumpTo}/>
+        <ReverseHistoryView onClick={this.handleReverseHistory}/>
+      </div>
+    );
+  }
+}
+
+class ReverseHistoryView extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="game-info">
+        <input type="button"  value="反序历史记录" onClick={this.props.onClick}/>
+      </div>
+    );
+  }
+}
+
+class Status extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getWinnerStatus() {
+    const winner = calculateWinner(this.props.currentSquares.squares);
+    let status;
+    if (winner) {
+      status = "winner: " + winner;
+    } else {
+      status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+    }
+    return status;
+  }
+
+  render() {
+    const status = this.getWinnerStatus();
+    return (
+      <div className="game-info">
+        <div>{status}</div>
+      </div>
+    )
+  }
+}
+
+class History extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let historyInfo;
+    const history = this.props.history;
+    if (this.props.historyAscOrder) {
+      historyInfo = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button
+              onClick={() => this.props.jumpTo(move)}
+              className={this.props.boldStep===move ? "selectHistory" : null}
+            >{desc}
+            </button>
+            <Coordinates move={move} step={step}/>
+          </li>
+        );
+      })
+    } else {
+      const len = history.length;
+      const tmp = history.slice(0,  history.length);
+      historyInfo = tmp.reverse().map((step, move) => {
+        const desc = step.xAxis!= null ?
+          'Go to move #' + (len - move -1) :
+          'Go to game start';
+        return (
+          <li key={(len - move -1)}>
+            <button
+              onClick={() => this.props.jumpTo((len - move -1))}
+              className={this.props.boldStep===(len - move -1) ? "selectHistory" : null}
+            >{desc}
+            </button>
+            <Coordinates move={(len - move -1)} step={step}/>
+          </li>
+        );
+      })
+    }
+    return (
+      <div className="game-info">
+        <ol>{historyInfo}</ol>
       </div>
     );
   }
@@ -134,12 +203,26 @@ class Game extends React.Component {
  * @return {null}
  */
 function Coordinates(props) {
-  if (props.move >0) {
+  if (props.step.xAxis != null) {
     return (
       <span>({props.step.xAxis}, {props.step.yAxis})</span>
     )
   }
   return null;
+}
+
+/**
+ * 方格组件
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 // ========================================
